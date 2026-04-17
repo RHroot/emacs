d71911a52809383b80a10ba1b561b892e36b9e81
Rishabh Haldiya <rishabhhaldiya18@proton.me>
2026-04-17 21:14:31 +0530

    i am just trying to make it better for myself to be reproucible i don't know how far i am getting but it is going
    Acked-by: Rishabh Haldiya <rishabhhaldiya18@proton.me>

--- init.el
+++ init.el
@@ -1,20 +1,19 @@
-(setq package-enable-at-startup nil)
+;; --- Redirect Custom Settings ---
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+(load custom-file 'noerror 'nomessage)
 
-;; --- Bootstrap straight.el (Modern Way) ---
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; --- Package Manager Setup ---
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-(straight-use-package 'use-package)
+;; --- Use-Package Setup ---
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
 (setq use-package-always-ensure t)
 
 ;; --- UI & Basic Settings ---
